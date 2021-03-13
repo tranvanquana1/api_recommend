9e9c7ba804fa4361f9a7ca6b4f72ceb4967da8de
tvQuan <tranvanquana1@gmail.com>
2021-03-13 13:32:58 +0700

    change HOST and POST

--- server.js
+++ server.js
@@ -60,6 +60,8 @@ app.use(() => {
 
 // Start server
 
-const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0'
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, HOST, () => console.log(`Server is running on ${HOST}:${PORT}`));
